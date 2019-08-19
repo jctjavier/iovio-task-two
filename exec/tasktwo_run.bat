1d0b74771e830b76ad11e6ff4dc56ab78a008c9c
jctjavier <joannecelina.javier@gmail.com>
2019-08-19 14:43:31 +0200

    Update tasktwo_run.bat

--- exec/tasktwo_run.bat
+++ exec/tasktwo_run.bat
@@ -1,8 +1,9 @@
-cls 
+ECHO OFF
+CLS 
 
-echo 'Bol.Com automation test starts now. Task 2 for IOVIO TC'
-echo 'by: jctjavier'
-
+ECHO 'Bol.Com automation test starts now. Task 2 for IOVIO TC'
+ECHO 'by: jctjavier'
+ECHO.
 git clone https://github.com/jctjavier/iovio-task-two.git
 cd iovio-task-two
 git fetch 
